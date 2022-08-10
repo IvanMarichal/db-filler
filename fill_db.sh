2e59cc7509f5b7eb832a428130e0093cfd38e51b
Ivan Marichal <ivanmarichalus@gmail.com>
2022-08-10 16:07:37 -0300

    Table subscription fixes

--- fill_db.sh
+++ fill_db.sh
@@ -1,39 +1,54 @@
-
-
-
-number_lines=0
-
 subscription () {
-    number_table_start=$(cat sql_script.txt | grep -n subscription | cut -c1)
-    number_table_end=0
-    i=2
-    while [[ $aux != "-" ]]
+    
+    for ((h = 0 ; h < $rows2 ; h++))
     do
-    aux=$(awk "NR==$(($number_table_start + $i))" sql_script.txt | cut -c1)
-    i=$(( $i + 1 ))
-    number_lines=$(( $number_lines + 1 ))
+        number_lines=0
+        number_table_start=$(cat sql_script.txt | grep -n subscription | cut -d ":" -f1 | head -1)
+        if [ -z "$number_table_start" ]
+        then
+        aux="-"
+        else
+        aux=" "
+        fi
+
+
+        i=3
+        while [[ $aux != "-" ]]
+        do
+        check_lines=$(( $number_table_start + $i ))
+        aux=$(awk "NR==$check_lines" sql_script.txt | cut -c1)
+        i=$(( $i + 1 ))
+        number_lines=$(( $number_lines + 1 ))
+        done
+
+
+
+        number_table_end=$(($number_table_start + $number_lines + 2))
+        if [ $number_lines -eq 0 ]
+        then
+        echo -e "\n--subscription\n-----------------------------------------------------------------------------------------\n\n\n-----------------------------------------------------------------------------------------\n" >> sql_script.txt
+        else
+        sed -i "$(($number_table_end - 1)) i INSERT INTO subscription (id, description) VALUES ($h, 'Lorem ipsum dolor sit amet')" sql_script.txt
+        fi
+
+
+
     done
-    echo "la cantidad de lineas es: $number_lines"
-    sleep 4
+
 }
 
 
 echo "1- Fill the whole database"
-echo "2- Fill certain tables"
 echo "0- Exit"
 read -p "Choose an option: " option
     case $option in
             1)read -p "How many rows: " rows
             touch sql_script.txt
+            rows2=$(($rows + 1))
             subscription
             sleep 2
             clear
-            sh fill_db.sh;;
-            2)read -p "Ingrese el numero del contacto: " tel
-            grep $tel usuarios.txt
-            sleep 7
-            clear
-            sh agenda.sh;;
+            exit;;
             0)exit;;
 
 
