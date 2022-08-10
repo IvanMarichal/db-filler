b81aeb580ff054a242617aaa83606f2782707e82
Ivan Marichal <ivanmarichalus@gmail.com>
2022-08-10 16:21:52 -0300

    Table subscription_type working

--- fill_db.sh
+++ fill_db.sh
@@ -1,7 +1,8 @@
 subscription () {
     
-    for ((h = 0 ; h < $rows2 ; h++))
+    for ((h = 0 ; h < $rows2 ; h++)) #repeat procces as many times as indicated
     do
+        #Checks where the table section starts
         number_lines=0
         number_table_start=$(cat sql_script.txt | grep -n subscription | cut -d ":" -f1 | head -1)
         if [ -z "$number_table_start" ]
@@ -11,7 +12,7 @@ subscription () {
         aux=" "
         fi
 
-
+        #checks where the table section ends
         i=3
         while [[ $aux != "-" ]]
         do
@@ -22,13 +23,53 @@ subscription () {
         done
 
 
-
+        #insert data
         number_table_end=$(($number_table_start + $number_lines + 2))
         if [ $number_lines -eq 0 ]
         then
         echo -e "\n--subscription\n-----------------------------------------------------------------------------------------\n\n\n-----------------------------------------------------------------------------------------\n" >> sql_script.txt
         else
         sed -i "$(($number_table_end - 1)) i INSERT INTO subscription (id, description) VALUES ($h, 'Lorem ipsum dolor sit amet')" sql_script.txt
+        fi
+
+
+
+    done
+
+}
+
+subscription_type () {
+    
+    for ((h = 0 ; h < $rows2 ; h++)) #repeat procces as many times as indicated
+    do
+        #Checks where the table section starts
+        number_lines=0
+        number_table_start=$(cat sql_script.txt | grep -n subscription_type | cut -d ":" -f1 | head -1)
+        if [ -z "$number_table_start" ]
+        then
+        aux="-"
+        else
+        aux=" "
+        fi
+
+        #checks where the table section ends
+        i=3
+        while [[ $aux != "-" ]]
+        do
+        check_lines=$(( $number_table_start + $i ))
+        aux=$(awk "NR==$check_lines" sql_script.txt | cut -c1)
+        i=$(( $i + 1 ))
+        number_lines=$(( $number_lines + 1 ))
+        done
+
+
+        #insert data
+        number_table_end=$(($number_table_start + $number_lines + 2))
+        if [ $number_lines -eq 0 ]
+        then
+        echo -e "\n--subscription_type\n-----------------------------------------------------------------------------------------\n\n\n-----------------------------------------------------------------------------------------\n" >> sql_script.txt
+        else
+        sed -i "$(($number_table_end - 1)) i INSERT INTO subscription (id, type) VALUES ($h, 'Lorem ipsum dolor sit amet')" sql_script.txt
         fi
 
 
@@ -46,6 +87,7 @@ read -p "Choose an option: " option
             touch sql_script.txt
             rows2=$(($rows + 1))
             subscription
+            subscription_type
             sleep 2
             clear
             exit;;
